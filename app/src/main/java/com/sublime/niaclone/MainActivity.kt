fd12b7187f7cdb10690b2aef96726f526fa8c76a
williamj <williamj949@gmail.com>
2023-12-22 15:26:23 -0500

    Added skeleton, using version catalog, updated .gitignore

---
+++ app/src/main/java/com/sublime/niaclone/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.sublime.niaclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.sublime.niaclone.ui.theme.NowInAndroidTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContent {
+            NowInAndroidTheme {
+                androidx.compose.material.Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    @Suppress("SameParameterValue")
+    @Composable
+    private fun Greeting(name: String) {
+        Text(text = "Hello $name! ")
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        NowInAndroidTheme {
+            Greeting("Android")
+        }
+    }
+
+}
